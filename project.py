ffbd755dce9c4908b1962a06a7d7e59fce4be70c
Michał Świdziński <michalswidzinski01@gmail.com>
2024-01-25 13:42:35 +0100

    Add new class, snake growth feature.

--- project.py
+++ project.py
@@ -10,11 +10,11 @@ clock = pygame.time.Clock()
 
 class Snake:
     def __init__(self):
-        self.x_position, self.y_position = TILE_SIZE, TILE_SIZE
+        self.x, self.y = TILE_SIZE, TILE_SIZE
         self.horizontal_direction = 1
         self.vertical_direction = 0
-        self.head = pygame.Rect(self.x_position, self.y_position, TILE_SIZE, TILE_SIZE)
-        self.body = [pygame.Rect(self.x_position - TILE_SIZE, self.y_position, TILE_SIZE, TILE_SIZE)]
+        self.head = pygame.Rect(self.x, self.y, TILE_SIZE, TILE_SIZE)
+        self.body = [pygame.Rect(self.x - TILE_SIZE, self.y, TILE_SIZE, TILE_SIZE)]
         self.dead = False
 
     def update_position(self):
@@ -25,7 +25,6 @@ class Snake:
         self.head.x += self.horizontal_direction * TILE_SIZE
         self.head.y += self.vertical_direction * TILE_SIZE
         self.body.remove(self.head)
-
 
     def snake_movement(self, keys):
         if keys[pygame.K_w] and self.vertical_direction != 1:
@@ -42,7 +41,13 @@ class Snake:
             self.vertical_direction = 0     
 
 class Opponent:
-    pass
+    def __init__(self):
+        self.x = int(random.randint(0, WINDOW) / TILE_SIZE) * TILE_SIZE
+        self.y = int(random.randint(0, WINDOW) / TILE_SIZE) * TILE_SIZE
+        self.rect = pygame.Rect(self.x, self.y, TILE_SIZE, TILE_SIZE)
+
+    def draw_opponent(self):
+        pygame.draw.rect(screen, "red", self.rect)
 
 def draw_grid():
     for  x_position in range(0, WINDOW, TILE_SIZE):
@@ -56,6 +61,7 @@ def draw_grid():
            
 draw_grid()
 snake = Snake()
+opponents = [Opponent() for opponent in range(3)]
 
 while True:
     for event in pygame.event.get():
@@ -63,7 +69,8 @@ while True:
             print("Thanks for playing!")
             exit()
 
-
+    
+            
     # Snake size and positon
     snake.snake_movement(pygame.key.get_pressed())
     snake.update_position()
@@ -76,7 +83,17 @@ while True:
     for tile in snake.body:
         pygame.draw.rect(screen, "green", tile)
 
-    
+    # Opponent position
+    for opponent in opponents:
+        opponent.draw_opponent()
+
+    for opponent in opponents:
+        if snake.head.x == opponent.x and snake.head.y == opponent.y:
+            snake.body.append(pygame.Rect(snake.head.x, snake.head.y, TILE_SIZE, TILE_SIZE))
+            opponents.remove(opponent)
+
+    if len(opponents) < 3:
+        opponents.append(Opponent())
 
     pygame.display.update()
     clock.tick(10)
